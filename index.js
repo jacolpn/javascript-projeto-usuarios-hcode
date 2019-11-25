b3c662fb1f2d1584edd12f78b8b77e3beaa065a0
jacolpn <jacolpn@outlook.com>
2019-11-25 17:33:40 -0300

    Configurado botão e imprimindo como JSON.

--- index.js
+++ index.js
@@ -6,9 +6,24 @@ var email = document.querySelector("#exampleInputEmail");
 var password = document.querySelector("#exampleInputPassword");
 var photo = document.querySelector("#exampleInputFile");
 var admin = document.querySelector("#exemploInputAdmin");
+var user = {};
+
 
 var fields = document.querySelectorAll("#form-user-create [name]");
 
-fields.forEach(function(field, index){
-    console.log(field.name, field.id);
+
+
+document.getElementById("form-user-create").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    fields.forEach(function(field, index){
+        if(field.name == "gender") {
+            if(field.checked) {
+                user[field.name] = field.value;
+            }
+        } else {
+            user[field.name] = field.value;
+        }
+    });
+    console.log(user);
 });
