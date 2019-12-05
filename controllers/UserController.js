00a52236e004c9f9207a63e759040e5182707176
jacolpn <jacolpn@outlook.com>
2019-12-05 17:05:34 -0300

    Ajustado problema ao enviar imagem e novas linhas na tabela.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -8,7 +8,36 @@ class UserController {
   onSubmit() {
     this.formEl.addEventListener("submit", event => {
       event.preventDefault();
-      this.addLine(this.getValues());
+      let values = this.getValues();
+      this.getPhoto().then((content) => {
+        values.photo = content;
+        this.addLine(values);
+      }, (e) => {
+        console.error(e);
+      });
+    });
+  }
+
+  getPhoto() {
+    return new Promise((resolve, reject) => {
+      let fileReader = new FileReader();
+      let elements = [...this.formEl.elements].filter(item => {
+        if(item.name === 'photo') {
+          return item;
+        }
+      })
+      let file = elements[0].files[0];
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+      fileReader.onerror = (e) => {
+        reject(e);
+      }
+      if(file) {
+        fileReader.readAsDataURL(file);
+      } else {
+        resolve('dist/img/boxed-bg.jpg');
+      }
     });
   }
 
@@ -20,6 +49,8 @@ class UserController {
         if(field.checked) {
           user[field.name] = field.value;
         }
+      } else if(field.name == "admin") {
+        user[field.name] = field.checked;
       } else {
         user[field.name] = field.value;
       }
@@ -27,6 +58,7 @@ class UserController {
     return new User (
       user.name,
       user.gender,
+      user.birth,
       user.country,
       user.email,
       user.password,
@@ -36,20 +68,20 @@ class UserController {
   }
 
   addLine(dataUser) {
-    this.tableEl.innerHTML = `
-      <tr>
-        <td>
-          <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
-        </td>
-        <td>${dataUser.name}</td>
-        <td>${dataUser.email}</td>
-        <td>${dataUser.admin}</td>
-        <td>${dataUser.birth}</td>
-        <td>
-          <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-          <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-        </td>
-      </tr>
-    `
+    let tr = document.createElement('tr');
+    tr.innerHTML = `
+      <td>
+        <img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm">
+      </td>
+      <td>${dataUser.name}</td>
+      <td>${dataUser.email}</td>
+      <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+      <td>${dataUser.birth}</td>
+      <td>
+        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+      </td>
+  `;
+    this.tableEl.appendChild(tr); 
   }
 }
